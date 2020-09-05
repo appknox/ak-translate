defd5702f08bd6998517cc22be3270eaf5c578aa
Praseetha-KR <praseetha04@gmail.com>
2020-09-05 18:36:54 +0530

    Store with vulnerabilities

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,26 @@
-import { createStore } from "vuex";
+import Vue from "vue";
+import Vuex from "vuex";
+import VulnerabilityModel from "../types/VulnerabilityType";
 
-export default createStore({
-  state: {},
-  mutations: {},
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    vulnerabilities: Array<VulnerabilityModel>()
+  },
+  getters: {
+    getVulnerabilities: state => {
+      return state.vulnerabilities;
+    },
+    getVulnerability: (state) => (id: number) => {
+      return state.vulnerabilities.find(v => v.id === id);
+    }
+  },
+  mutations: {
+    saveVulnerability(state, payload) {
+      state.vulnerabilities.push(payload);
+    }
+  },
   actions: {},
   modules: {}
 });
---
+++ src/types/VulnerabilitiesType.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import VulnerabilityType from "./VulnerabilityType";
+
+
+export default interface VulnerabilitiesType {
+  [key: string]: VulnerabilityType;
+}
