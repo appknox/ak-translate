7e8840da673644f725e837c1efb7843e5212ac8b
Praseetha-KR <praseetha04@gmail.com>
2020-09-08 05:53:28 +0530

    Init with modified vulnerabilities

---
+++ src/types/VulnerabilitiesModifiedType.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import VulnerabilityType from "./VulnerabilityType";
+
+
+export interface VulnerabilityDiffType {
+  hash: string;
+  vulnerability: VulnerabilityType;
+}
+
+export interface IdVulnerabilityDiffMapType {
+  [key: string]: VulnerabilityDiffType;
+}
+
+export default interface VulnerabilitiesModifiedType {
+  [key: string]: IdVulnerabilityDiffMapType;
+}
